e86e9de0a44538b73f53dea68f769875bd0618f6
phuocvinh123 <doanvanphuocvinh1510@gmail.com>
2024-04-03 10:04:24 +0700

    bai 10

--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -1,37 +1,50 @@
 import { User } from "src/auth/user.entity";
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Attendee } from "./attendee.entity";
+import { Expose } from "class-transformer";
 
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
+  @Expose()
   id: number;
 
   @Column()
+  @Expose()
   name: string;
 
   @Column()
+  @Expose()
   description: string;
 
   @Column()
+  @Expose()
   when: Date;
 
   @Column()
+  @Expose()
   address: string;
 
   @OneToMany(() => Attendee, (attendee) => attendee.event, {
     cascade: true
   })
+  @Expose()
   attendees: Attendee[];
 
   @ManyToOne(() => User, (user) => user.organized)
+  @Expose()
   organizer: User;
 
   @Column({ nullable: true })
+  @Expose()
   organizerId: number;
 
+  @Expose()
   attendeeCount?: number;
+  @Expose()
   attendeeRejected?: number;
+  @Expose()
   attendeeMaybe?: number;
+  @Expose()
   attendeeAccepted?: number;
 }
